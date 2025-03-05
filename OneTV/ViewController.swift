448c7b941e158de4f7f20c3083dc0eec263b1814
botanPro <botanamedi8@gmail.com>
2025-03-06 00:12:10 +0300

    Initial Commit

---
+++ OneTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneTV
+//
+//  Created by Botan Amedi on 06/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
